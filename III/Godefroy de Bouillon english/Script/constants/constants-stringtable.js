aacf599b14cfef014291bf46fa2a3733e2dbda88
Lucas Jourdain <lucasjourdain25@gmail.com>
2025-08-02 16:13:31 +0200

    3ème jeu(sur Godefroy de Bouillon) de la série ajouté

---
+++ III/Godefroy de Bouillon english/Script/constants/constants-stringtable.js
@@ -0,0 +1,335 @@
+
+var StringTable = {
+	LoadSave_SaveQuestion: "Sauver dans ce fichier ?",
+	LoadSave_NoLoad: "Pas de fichier à charger",
+	LoadSave_NoData: "no data",
+	LoadSave_Chapter: "Chapitre",
+	LoadSave_SaveFileMark: "Fichier",
+	LoadSave_Rest: "Camp",
+	
+	Communication_NoData: "Aucune donnée",
+	
+	GameStart_DifficultySelect: "Choisissez la difficulté du jeu",
+	GameStart_ClearPointDescription: "Vous pouvez acquérir des Objets contre des Points d'accomplissement.\nRecommandé si vous voulez commencer le jeu à un niveau élevé.\nVos Points actuels:",
+	GameStart_ClearPointQuestion: "Acquérir Objet ?",
+	
+	UnitSortie_Start: "Press Space",
+	UnitSortie_Max: "Nb max d'Unités en Sortie",
+	UnitSortie_Question: "Faire une Sortie ?",
+	
+	Stock_Extract: "Prendre Objet",
+	Stock_Store: "Ranger Objet",
+	Stock_AllStore: "Ranger tous les Objets",
+	
+	Marshal_StockOperation: "Changer d'Unité via flèches haute/basse",
+	Marshal_StockTrade: "Prendre Objet obtenu",
+	Marshal_ItemTrade: "Echanger Objets entre Unités",
+	Marshal_UnitSort: "Repositionner les Unités",
+	Marshal_UnitStatus: "Checker Statut de l'Unité",
+	Marshal_Shop: "Acheter Objet",
+	Marshal_Bonus: "Echanger Bonus contre Objets",
+	Marshal_ItemUse: "Utiliser Objet",
+	Marshal_ClassChange: "Changer Classe",
+	
+	ItemUse_Unit: "Unité",
+	ItemUse_Stock: "Réserve",
+	ItemUse_Question: "Utiliser cet Objet ?",
+	
+	ExperienceDistribution_CannotLevelup: "Expérience",
+	ExperienceDistribution_BonusShortage: "Impossible de passer au niveau supérieur",
+	
+	Extra_Character: "Les Persos vus dans le Jeu sont listés",
+	Extra_Word: "Les termes techniques vus dans le jeu sont listés",
+	Extra_Gallery: "Les Images vues dans le jeu sont listées",
+	Extra_SoundRoom: "Les musiques jouées dans le jeu sont listées",
+	Extra_Recollection: "Les scènes vues dans le jeu sont listées",
+	
+	Config_MusicPlay: "Volume BGM",
+	Config_MusicPlayDescription: "Régler Volume BGM",
+	Config_SoundEffect: "Vol. Effets",
+	Config_SoundEffectDescription: "Régler Vol. Effets",
+	Config_RealBattle: "Combat réel",
+	Config_RealBattleDescription: "Réglage des animations en combat",
+	Config_AutoTurnEnd: "Fin de tour auto",
+	Config_AutoTurnEndDescription: "Réglage Fin de tour auto",
+	Config_AutoTurnSkip: "Zapper tour Ennemi",
+	Config_AutoTurnSkipDescription: "Réglage saut du tour Ennemi",
+	Config_MapGrid: "Grille de Carte",
+	Config_MapGridDescription: "Affichage Grille de Carte",
+	Config_UnitSpeed: "Vitesse Unité",
+	Config_UnitSpeedDescription: "Réglage Vitesse Unité",
+	Config_MessageSpeed: "Vitesse Messages",
+	Config_MessageSpeedDescription: "Réglage Vitesse texte",
+	Config_MapUnitWindow: "Fenêtre Unités",
+	Config_MapUnitWindowDescription: "Affichage Fenêtre détaillée des Unités",
+	Config_LoadCommand: "Commande Chargement",
+	Config_LoadCommandDescription: "Afficher Commande Chargem. en Combat",
+	Config_AutoCursor: "Curseur auto",
+	Config_AutoCursorDescription: "Placer le Curseur sur le leader",
+	Config_MouseOperation: "Souris activée",
+	Config_MouseOperationDescription: "Utilisation Souris",
+	Config_MouseCursorTracking: "Suivi du Curseur",
+	Config_MouseCursorTrackingDescription: "Autoriser le Suivi auto du Curseur",
+	Config_Voice: "Volume Voix",
+	Config_VoiceDescription: "Réglage Vol. Voix",
+	Config_RealBattleScaling: "Ecran Combat réel élargi",
+	Config_RealBattleScalingDescription: "Réglage écran Combat réel élargi",
+	Config_ScrollSpeed: "Vitesse de scroll",
+	Config_ScrollSpeedDescription: "Réglage Vitesse de scroll des Unités",
+	Config_EnemyMarking: "Champ d'action ennemi",
+	Config_EnemyMarkingDescription: "Montrer champ d'action sur Carte en cas d'Annulation",
+	Config_MapUnitHpVisible: "PV visibles sur la Carte",
+	Config_MapUnitHpVisibleDescription: "Affichage des PV sur la Carte",
+	Config_MapUnitSymbol: "Symboles sur Carte",
+	Config_MapUnitSymbolDescription: "Affichage de l'Allégeance des Unités",
+	Config_DamagePopup: "Pop-up Dommages",
+	Config_DamagePopupDescription: "Afficher Dommages causés en Combat",
+	Config_SkipControl: "Impossible de zapper",
+	Config_SkipControlDescription: "Réglage des possibilités de zapping",
+	
+	ItemChange_UnitItemFull: "L'inventaire de l'unité est plein.\nAppuyez sur OK ou Annuler",
+	ItemChange_TradeTitle: "Echanger cet Objet ?",
+	ItemChange_StockSendTitle: "Envoyer en Réserve l'Objet obtenu ?",
+	ItemChange_StockThrowTitle: "Jeter l'Objet obtenu ?",
+	ItemChange_StockItemFull: "La Réserve est pleine.\nVendez ou jetez des Objets",
+	ItemChange_TargetItem: "Objet cible",
+	
+	BattleSetup_NoPlayer: "Il n'y a plus de joueur, Game Over",
+	
+	BattleResult_GetGold: "Or",
+	BattleResult_GetBonus: "Bonus",
+	BattleResult_GetItem: "Objets",
+	
+	Rest_Next: "Lever le Camp ?",
+	
+	Quest_Select: "Poursuivre vers cette Carte ?",
+	Quest_EnemyTotal: "Ennemis",
+	Quest_AverageLevel: "Niv. Moyen ennemi",
+	Quest_GetGold: "Or",
+	Quest_GetBonus: "Bonus",
+	Quest_GetItem: "Get!",
+	
+	ClassChange_UnitClassChange: "Changer pour cette Classe ?",
+	ClassChange_UnableClassChange: "Cette Unité ne peut pas changer de Classe",
+	ClassChange_UnableClassChangeMore: "Cette Unité ne peut plus changer de Classe",
+	ClassChange_UnableClassChangeItem: "Cette Unité ne peut pas changer de Classe avec cet Objet",
+	
+	Metamorphoze_Change: "Métamorphoser cette Unité ?",
+	
+	ShopLayout_SelectBuy: "Acheter",
+	ShopLayout_SelectSell: "Vendre",
+	ShopLayout_Unit: "Unité",
+	ShopLayout_Stock: "Réserve",
+	
+	ResurrectionLayout_Empty: "Pas d'Unité à ranimer",
+	ResurrectionLayout_Question: "Ranimer cette Unité ?",
+	
+	UnitSummary_Alive: "Vivant",
+	UnitSummary_Death: "Mort",
+	UnitSummary_Injury: "Blessé",
+	UnitSummary_Erase: "Eliminé",
+	UnitSummary_Immortal: "Immorte",
+	
+	SkillList_Owner: "Possesseur",
+	
+	EventTest_End: "L'événement de Souvenir est terminé.",
+	EventTest_Key: "Fermer en cliquant OK ou Annuler",
+	
+	QuestionWindow_DefaultCase1: "Oui",
+	QuestionWindow_DefaultCase2: "Non",
+	
+	InfoWindow_Information: "- Notice -",
+	InfoWindow_Warning: "- Warning -",
+	
+	ReverseWeapon_Magic: "Arme magique",
+	ReverseWeapon_Physics: "Arme physique",
+	
+	MapPosChoose_Unit: "Confirmer cette Unité ?",
+	MapPosChoose_Pos: "Confirmer cette Position ?",
+	
+	Discard_Warning: "Cet Objet n'est pas jetable",
+	
+	StockItem_ItemDiscard: "Jeter cet Objet ?",
+	StockItem_Extract: "Prendre",
+	StockItem_Store: "Ranger",
+	StockItem_AllStore: "Vider",
+	
+	ItemWork_Equipment: "Equiper",
+	ItemWork_Use: "Utiliser",
+	ItemWork_Discard: "Jeter",
+	
+	ItemInfo_Recovery: "Guérison",
+	ItemInfo_EntireRecovery: "Guérison de groupe",
+	ItemInfo_Damage: "Dommages",
+	ItemInfo_ClassChange: "Changem. Classe",
+	ItemInfo_Doping: "Boost",
+	ItemInfo_Resurrection: "Résurrection",
+	ItemInfo_SkillChange: "Ajout de Compétence",
+	ItemInfo_Quick: "Réactivation",
+	ItemInfo_Teleportation: "Téléportation",
+	ItemInfo_Rescue: "Secours",
+	ItemInfo_Key: "Ouvrir",
+	ItemInfo_Durability: "Réparation",
+	ItemInfo_Steal: "Vol",
+	ItemInfo_State: "Altérer Statut",
+	ItemInfo_StateRecovery: "Rétablir Statut",
+	ItemInfo_Switch: "Spécial",
+	ItemInfo_Fusion: "Fusion",
+	ItemInfo_Metamorphoze: "Métamorphose",
+	ItemInfo_Unusable: "Inutilisable",
+	ItemInfo_Custom: "Customisé",
+	
+	ItemWord_SuffixItem: " (Objet)",
+	ItemWord_SuffixWand: " (Bâton)",
+	ItemWord_MultiAttack: " d'Attaques",
+	
+	SkillWord_Less: "% ou moins",
+	SkillWord_Invocation: "Taux",
+	FusionWord_Success: "Taux succès",
+	
+	UnitSentence_Support: "-Soutien-",
+	
+	UnitType_Player: "JOUEUR",
+	UnitType_Enemy: "Ennemi",
+	UnitType_Ally: "Allié",
+	
+	WeaponOption_HpAbsorb: "Drain de PV",
+	WeaponOption_NoGuard: "Ignore Déf",
+	WeaponOption_HpMinimum: "Réduit PV à 1",
+	WeaponOption_HalveAttack: "Attaque/2",
+	WeaponOption_HalveAttackBreak: "Annule Attaque/2",
+	WeaponOption_SealAttack: "Attaque scellée",
+	WeaponOption_SealAttackBreak: "Annule Attaque scellée",
+	
+	DamageType_Fixed: "Fixé",
+	DamageType_Physics: "Physique",
+	DamageType_Magic: "Magique",
+	DamageType_Name: "Attribut",
+	
+	DurabilityType_Max: "Tout guérir",
+	DurabilityType_Half: "Div. par 2",
+	DurabilityType_Break: "Cassé",
+	
+	Chapter_Header: "Ch",
+	Chapter_Footer: "",
+	Chapter_SideStory: "ss",
+	Chapter_First: "Prologue",
+	Chapter_Last: "Final",
+	Chapter_Rest: "Terminé",
+	Chapter_Quest: "Quête",
+	
+	Spped_Direct: "Instantané",
+	Speed_High: "Vite",
+	Speed_Normal: "Normal",
+	Speed_Low: "Lent",
+	
+	AutoTurnSkip_Direct: "Instant",
+	AutoTurnSkip_Quick: "Accéléré",
+	AutoTurnSkip_None: "Rien",
+	
+	SkipControl_AllInput: "Tous",
+	SkipControl_Mouse: "Souris",
+	SkipControl_None: "Rien",
+	
+	MapUnitHp_Number: "Valeur",
+	MapUnitHp_Gauge: "Jauge",
+	MapUnitHp_None: "Rien",
+	
+	Objective_Victory: "Conditions de Victoire",
+	Objective_Defeat: "Conditions de Défaite",
+	
+	Select_On: "On",
+	Select_Off: "Off",
+	
+	Range_Self: "Simple",
+	Range_All: "Tout",
+	
+	Recovery_Value: "PT",
+	Recovery_All: "Tout guérir",
+	
+	Aggregation_SuffixEquipment: "Equip",
+	Aggregation_SuffixPossession: "Posses",
+	Aggregation_SuffixAddition: " Addition",
+	Aggregation_Match: "Efficace",
+	Aggregation_Mismatch: "Inefficace",
+	Aggregation_MatchAll: "Tout efficace",
+	Aggregation_MismatchAll: "Rien d'efficace",
+	
+	Key_Target: "Cibles",
+	Key_Gate: "Porte",
+	Key_Treasure: "Coffre",
+	
+	ItemLost: ' détruit',
+	ItemSteal: ' volé',
+	
+	ClassChangeInfo_Target: "Cibles",
+	QuickInfo_Surroundings: 'Alentours',
+	Teleportation_Range: "Dis",
+	
+	SkillCategory_BattleAttack: "Type Attaque en Combat",
+	SkillCategory_BattleDefence: "Type Défense en Combat",
+	SkillCategory_BattleAllowed: "Type Autorisé en Combat",
+	SkillCategory_Allowed: "Type Autorisation",
+	SkillCategory_Command: "Type Commande",
+	SkillCategory_Action: "Type Action",
+	SkillCategory_Custom: "Customisé",
+	
+	GetTitle_ItemChange: "Gain Objet!",
+	GetTitle_SkillChange: "Gain Compét.!",
+	GetTitle_GoldChange: "Gain Or!",
+	GetTitle_BonusChange: "Gain Bonus!",
+	
+	LostTitle_ItemChange: "Objet perdu!",
+	LostTitle_SkillChange: "Compét. perdue",
+	LostTitle_GoldChange: "Or perdu!",
+	LostTitle_BonusChange: "Bonus perdu!",
+	
+	GetTitle_ExperiencePlus: "Gain Exp!",
+	GetTitle_NewSkill: "Nouv. Compét.!",
+	GetTitle_PowerupSkill: "Gain de Puiss.!",
+	
+	UnitSimple_Target: "Cible ...",
+	
+	State_Addition: "Addition",
+	State_Recovery: "Soin",
+	State_Regist: "Résistance",
+	State_AllBadState: "Toutes Altérations",
+	
+	HideData_Secret: "-secret-",
+	HideData_Question: "???",
+	HideData_Unknown: "Inconnu",
+	
+	AttackMenu_AttackCount: "Nb attaques ",
+	
+	PlayerActive: "Joueur",
+	
+	SkipProgress: 'Sauter ...',
+	
+	PlayTime: 'Play Time',
+	
+	Status_Level: "Niv",
+	Status_Experience: "Ex",
+	
+	Signal_Turn: "TOUR",
+	Signal_Gold: "OR",
+	Signal_Bonus: "BONUS",
+	
+	CurrencySign_Gold: "O",
+	CurrencySign_Bonus: "B",
+	CurrencySign_Point: "P",
+	
+	SignWord_Multiple: 'x',
+	SignWord_Percent: '%',
+	SignWord_WaveDash: 'à',
+	SignWord_Limitless: '--'
+};
+
+var ContentLayout = {
+	ITEM_SPACE: 62,
+	KEYWORD_HEIGHT: 2
+};
+
+var HorizontalLayout = {
+	OBJECT_SPACE: 5,
+	OBJECT_WIDTH: 14
+};
